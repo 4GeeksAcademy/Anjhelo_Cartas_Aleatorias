b20836e401255d3b639df9ad80a5e4d110c91b44
LOHAN2000 <160059524+LOHAN2000@users.noreply.github.com>
2024-08-04 16:57:30 +0000

    html-completo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let type = ["type-heart", "type-spades", "type-diamond", "type-clubs"];
+  let number = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function random(arr) {
+    return Math.floor(Math.random() * arr.length);
+  }
+
+  let list = document.getElementsByClassName("type");
+  Array.from(list).forEach(element => {
+    element.parentNode.removeChild(element);
+  });
 };
